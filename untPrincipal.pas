21c5cd085ed7a5594f41351f2934a256f4fd56cf
fabiocotta@fabiocotta.com.br <fabiocotta>
2024-03-18 21:26:47 -0300

    segundo commit
    atualizações

--- untPrincipal.pas
+++ untPrincipal.pas
@@ -4,10 +4,17 @@ interface
 
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
-  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs;
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.Objects,
+  FMX.Controls.Presentation, FMX.StdCtrls, FMX.ListView.Types,
+  FMX.ListView.Appearances, FMX.ListView.Adapters.Base, FMX.ListView;
 
 type
   TfrmPrincipal = class(TForm)
+    recToolbar: TRectangle;
+    imgMenu: TImage;
+    img: TImage;
+    lblTitulo: TLabel;
+    listPrincipal: TListView;
   private
     { Private declarations }
   public
