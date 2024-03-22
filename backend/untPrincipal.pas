709aa1917b84715258554172d7f1b7b37a9204de
fabiocotta@fabiocotta.com.br <fabiocotta>
2024-03-21 22:36:35 -0300

    atualização de arquivos
    criando o banck

---
+++ backend/untPrincipal.pas
@@ -0,0 +1,27 @@
+unit untPrincipal;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.StdCtrls,
+  FMX.Controls.Presentation;
+
+type
+  TfrmPrincipal = class(TForm)
+    Switch1: TSwitch;
+    Label1: TLabel;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  frmPrincipal: TfrmPrincipal;
+
+implementation
+
+{$R *.fmx}
+
+end.
